e5565f65a57bb70427bef710432a6450b0990a4d
acedeywin <stephenokpala@gmail.com>
2019-10-25 21:59:43 +0100

    design update

--- script.js
+++ script.js
@@ -1,108 +1,72 @@
+'use strict';
+//Wrap all codes in an Immediately Invoked Function Expression (IIFE).
+(() => {
 
-      const users = [];
+  const displaySelectedUser = () => {
 
-      const computeBMI = ({
-        weight,
-        height,
-        country
-      }) => {
-        let countries = ['Chad', 'Sierra Leone', 'Mali', 'Gambia',
-'Uganda', 'Ghana', 'Senegal', 'Somalia',
-          'Ivory Coast', 'Isreal'
-        ];
-        const h = height * 0.3048;
-        let bmi = weight / (h * h);
-        for (let i = 0; i < countries.length; i++) {
-          if (countries[i].toLowerCase() === country.toLowerCase()) {
-            bmi = bmi * 0.82;
-          }
-        }
-        return bmi.toFixed(1);
+    const userAge = document.querySelector('#age').value,
+          userName = document.querySelector('#name').value,
+          userHeight = document.querySelector('#height').value,
+          userWeight = document.querySelector('#weight').value,
+          userSex = document.querySelector('#sex').value,
+          userCountry = document.querySelector('#country').value,
+          message = document.querySelector('#message');
+
+    const computeBMI = (height, weight) => {
+
+      const h = height * 0.3048;
+      let bmi = weight / (h * h);
+        
+      return bmi.toFixed(1);
+    };
+
+    if(userName == '' || userAge == '' || userHeight == '' || userWeight == '' || userSex == '' || userCountry == ''){
+
+      message.innerText = 'Please enter a valid input.'
+    }else{
+
+      document.querySelector('.data-age').innerText += ` ${userAge}`;
+      document.querySelector('.data-height').innerText += ` ${userHeight}`;
+      document.querySelector('.data-weight').innerText += ` ${userWeight}`;
+      document.querySelector('.data-sex').innerText += ` ${userSex.toUpperCase()}`;
+      document.querySelector('.data-name').innerText += ` ${userName.toUpperCase()}`;
+      document.querySelector('.data-country').innerText += ` ${userCountry.toUpperCase()}`;
+      
+      let bmi = computeBMI(userHeight, userWeight);
+
+      document.querySelector('#display').innerText = bmi;
+
+      message.innerText = `Dear ${userName} from ${userCountry}, your Body Mass Index is ${bmi}. Thank you.`
+
+      const myForm = document.querySelector('#my-form');
+      myForm.reset();
+
+      const elements = myForm.elements;
+
+      for(var i = 0, len = elements.length; i < len; ++i){
+        elements[i].disabled = true;
       }
+    }  
 
-      const getSelectedUser = (userId) => {
-        return users.find(({
-          id
-        }) => id === userId);
-      }
+  }; 
 
-      const displaySelectedUser = ({
-        target
-      }) => {
-        let user = getSelectedUser(target.value);
+  const reset = document.querySelector('#reset');
+  reset.style.display = 'none';
+    
+  const powerupTheUI = () => {
+    const btn = document.querySelector("#oracle");
 
-        let properties = Object.keys(user);
-        properties.forEach(prop => {
-          let element = document.querySelector(`span[data-${prop}-value]`);
-          if (element) {
-            element.textContent = user[prop]
-          }
-        });
-      }
+    btn.addEventListener('click',() =>{
+      displaySelectedUser();
 
-      const letsCalculateBMI = () => {
-        let selected = document.querySelector("select").value;
+      reset.style.display = 'inline';
+      
+    });
+        
+  }
 
-        let user = getSelectedUser(selected);
+  powerupTheUI();
+  
+})();
 
-        let bmi = computeBMI(user);
-        let outcome = document.querySelector("#outcome");
-        outcome.querySelector('p').textContent = bmi;
-      }
-
-      const powerupTheUI = () => {
-        document.querySelector("select").addEventListener('change',
-displaySelectedUser);
-        document.querySelector("#oracle").addEventListener('click',
-letsCalculateBMI)
-      }
-
-      const displayUsers = (users) => {
-        users.forEach(user => {
-          let newUser = document.createElement("option");
-          newUser.value = user.id;
-          newUser.textContent = user.name;
-          document.querySelector("select").appendChild(newUser);
-        });
-      }
-
-      const fetchAndDisplayUsers = () => {
-        const api =
-'https://randomapi.com/api/y1lfp11q?key=LEIX-GF3O-AG7I-6J84';
-        fetch(api).then(result => result.json()).then(({
-          results
-        }) => {
-          const [user] = results;
-          users.push(user);
-          displayUsers([user]);
-        });
-        users.push({
-          age: 40,
-          weight: 75,
-          height: 6,
-          gender: 'Male',
-          country: 'Nigeria',
-          name: 'Charles Odili',
-          id: 'dfhb454768DghtF'
-        });
-        users.push({
-          age: 30,
-          weight: 67,
-          height: 6,
-          country: 'China',
-          gender: 'Male',
-          name: 'Daniel Kigali',
-          id: 'psbf1533400803'
-        });
-
-        displayUsers(users);
-      };
-
-      const startApp = () => {
-        powerupTheUI();
-        fetchAndDisplayUsers();
-      };
-
-      startApp();
-
-   
+   //javascript:location.reload(true)
